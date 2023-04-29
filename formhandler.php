9fc42a790fc1243561c3dfd3dc656967f88a9a9c
UZRKHATRI29 <108521311+UZRKHATRI29@users.noreply.github.com>
2023-04-29 18:57:22 +0530

    Add files via upload

---
+++ formhandler.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'royalconsultantae@gmail.com';
+
+$email_subject = 'your Query';
+
+$email_body = "User Name: $name.\n".    
+
+              "User Email: $visitor_email.\n".                          
+
+              "subject: $subject.\n".
+        
+              "User Message: $visitor_email.\n";
+
+$to = 'uzerkhatri812@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-To:  $visitor_email \r\n ";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+
+?>
